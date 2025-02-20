6aae6b0bd50c079341f1f27f46360374271b7618
TairB <ttair47@gmail.com>
2025-02-20 14:30:00 +0500

    Initial commit

--- mickeys_clock/clock.py
+++ mickeys_clock/clock.py
@@ -10,20 +10,16 @@ class MickeyClock:
         self.h = height
         self.center = (width // 2, height // 2)
 
-        # Загружаем руки
+        
         self.mh = pygame.image.load("images/right_hand.png").convert_alpha()
         self.sh = pygame.image.load("images/left_hand.png").convert_alpha()
 
-        # Масштабируем
+        
         self.mh = pygame.transform.scale(self.mh, (35, 170))
         self.sh = pygame.transform.scale(self.sh, (35, 170))
 
     def rab(self, image, angle, pos):
-        """
-        Вращает изображение и рисует с центром в pos.
-        angle = -6 * value (360 / 60 = 6 градусов за единицу)
-        минус = по часовой стрелке
-        """
+   
         ri = pygame.transform.rotate(image, angle)
         nr = ri.get_rect(center=pos)
         self.screen.blit(ri, nr.topleft)
